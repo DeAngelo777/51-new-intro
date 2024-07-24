d0572a87fe623fca2a5612d53cc5e6531b215f13
Laimonas <ldirgelas530@gmail.com>
2024-07-24 11:35:38 +0300

    Create .gitignore

---
+++ index.js
@@ -0,0 +1,51 @@
+let m = 0 ;
+
+console.log(m);
+console.log(m++); // 0 
+console.log(m++); // 1
+
+
+let i = 0;
+
+console.log(i);
+
+i = i + 1;
+
+console.log(i);
+
+console.clear();
+
+
+let amzius = 80;
+
+amzius = amzius + 1;
+
+amzius += 1;
+
+++amzius;
+
+console.log(amzius);
+
+console.clear();
+
+let skola = 55;
+
+skola = skola -1;
+
+console.log(skola);
+
+skola - skola - 1;
+
+console.log(skola);
+
+skola--;
+
+console.log(skola);
+
+
+--skola;
+
+console.log(skola);
+
+console.clear();
+
